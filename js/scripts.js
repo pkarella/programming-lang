7f224a6aa1dfdf39e2370addd8e5ff3fc905b301
peter <pkarella27@yahoo.com>
2017-03-24 09:30:24 -0700

    add css styling,create several form elements for html

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(
+  function(){
+    $("#info-form").submit(function(event)){
+    event.preventDefault();
+    var years = $parseInt(("#time").val();
+    var int = $parseInt(("#interactive").val();
+    var  = $parseInt(("#size").val();
+    var  = $parseInt(("#challenge").val();
+
+    var first= if (age >= 2);
+    
+};
+
+
+
+
+
+  // if ((time <= 2)||(time >2);
+  //   alert("Equilateral: All sides are equal.")
+  });
+});
+//   else if ((sideOne + sideTwo <= sideThree) || (sideOne + sideThree <= sideTwo) || (sideTwo + sideThree <= sideOne)) {
+//     alert("NOT a triangle: The sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side.")
+//   }
+//   else if (((sideOne === sideTwo) && (sideOne !== sideThree)) || ((sideOne === sideThree) && (sideOne !== sideTwo)) || ((sideTwo === sideThree) && (sideOne !== sideTwo))) {
+//     alert("Isosceles: Exactly 2 sides are equal.")
+//   }
+//   else if ((sideOne !== sideTwo) && (sideOne !== sideThree) && (sideTwo !== sideThree)) {
+//     alert("Scalene: No sides are equal.")
+//   }
+// )
