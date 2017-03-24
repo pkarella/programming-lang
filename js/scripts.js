8c098b9d0fea3d5f6e4a883574905af723cc66cd
peter <pkarella27@yahoo.com>
2017-03-24 10:50:38 -0700

    continue to refactor code. Add more radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,34 @@
+
+// var result=function(time,interactive,size,challenge){
+//   if
+// }
+
+
+
+
+
 $(document).ready(
   function(){
     $("#info-form").submit(function(event)){
     event.preventDefault();
-    var years = $parseInt(("#time").val();
-    var int = $parseInt(("#interactive").val();
-    var  = $parseInt(("#size").val();
-    var  = $parseInt(("#challenge").val();
-
-    var first= if (age >= 2);
-    
-};
-
-
+    var int =("#interactive").val("interact1");
+    var siz =("#size").val("size1");
+    var wolf= if(("interact1#interactive" === "size1#size")){
+      ("wolf").show("#answer1")
+    };
+    });
+    };
+    // var first= if (age >= 2);
 
 
 
-  // if ((time <= 2)||(time >2);
+// var years = $parseInt(("#time").val();
+
+// var  chall = $parseInt(("#challenge").val();
+
+
   //   alert("Equilateral: All sides are equal.")
-  });
-});
+
 //   else if ((sideOne + sideTwo <= sideThree) || (sideOne + sideThree <= sideTwo) || (sideTwo + sideThree <= sideOne)) {
 //     alert("NOT a triangle: The sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side.")
 //   }
