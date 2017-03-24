952cf6575f7a60c52f4241f97fa30b3450af8633
peter <pkarella27@yahoo.com>
2017-03-24 12:47:45 -0700

    try to figure this out

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,26 @@
 
-// var result=function(time,interactive,size,challenge){
-//   if
-// }
-
-
-
-
-
 $(document).ready(
-  function(){
-    $("#info-form").submit(function(event)){
-    event.preventDefault();
-    var int =("#interactive").val("interact1");
-    var siz =("#size").val("size1");
-    var wolf= if(("interact1#interactive" === "size1#size")){
-      ("wolf").show("#answer1")
-    };
+  function() {
+    $("#info-form").submit(function(event) {
+
+      var size = $("input:radio[name=size]:checked").val();
+      var check1 = $("#check1").val();
+
+      if ("size" === "yes" && "check1" === "hell" || "check1" === "heaven"){
+        $("col-md-4").text("help");
+        event.preventDefault();
+      });
     });
-    };
-    // var first= if (age >= 2);
-
-
-
-// var years = $parseInt(("#time").val();
-
-// var  chall = $parseInt(("#challenge").val();
-
-
-  //   alert("Equilateral: All sides are equal.")
-
-//   else if ((sideOne + sideTwo <= sideThree) || (sideOne + sideThree <= sideTwo) || (sideTwo + sideThree <= sideOne)) {
-//     alert("NOT a triangle: The sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side.")
-//   }
-//   else if (((sideOne === sideTwo) && (sideOne !== sideThree)) || ((sideOne === sideThree) && (sideOne !== sideTwo)) || ((sideTwo === sideThree) && (sideOne !== sideTwo))) {
-//     alert("Isosceles: Exactly 2 sides are equal.")
-//   }
-//   else if ((sideOne !== sideTwo) && (sideOne !== sideThree) && (sideTwo !== sideThree)) {
-//     alert("Scalene: No sides are equal.")
-//   }
-// )
+  );
+  // var dob = $("#born").val();
+// if (size === "no" && sign === "Aries" || sign === "Taurus") {
+//   $("h2").text("Tom Cruise");
+// };
+//
+// if (size === "dogs" && sign === "Leo") {
+//   $("h2").text("Brad Pitt");
+// };
+//
+// if (size === "nopets") {
+//   $("h2").text("Donald Trump");
+// };
