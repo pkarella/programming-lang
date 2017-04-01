f1a76dec8308ee5071043b2fbf3e23bc40b47a12
peter <pkarella27@yahoo.com>
2017-03-31 17:07:06 -0700

    fix code finalize style

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,20 @@ $(document).ready(
   function() {
     $("#info-form").submit(function(event) {
       event.preventDefault();
+      var times = $("#time").val();
       var sizes = $("input:radio[name=size]:checked").val();
       var checksa = $("#check1").val();
       var checksb = $("#check2").val();
       var checksc = $("#check3").val();
-      if (sizes === "websites" && checksa === "Challenge" && checksb === "Very Important" &&  checksc === "I love security"|| checksb === "Somewhat" && checksc === "Eh.."){
+
+      if (times <=2 && sizes == "websites" && checksa =="Challenge" && checksb == "Very Important" &&  checksc == "I love security"){
         $("#php").toggle();
-      };
-      if (sizes === "Larger Platforms" && checksa === "Challenge" && checksb === "Very Important" &&  checksc === "I love security"){
+      }
+      else if (times == 3 && sizes == "larger" && checksa == "Challenge" && checksb == "Very Important" &&  checksc === "I love security"){
         $("#java").toggle();
-      };
-      if (sizes === "I'm open" && checksa === "Challenge" && checksb === "Very Important" &&  checksc === "I love security"){
+      }
+      else if (times >= 4 && sizes == "open" && checksa == "Challenge" && checksb == "Very Important" &&  checksc == "I love security"){
         $("#ruby").toggle();
-      };
+      }
   });
 });
